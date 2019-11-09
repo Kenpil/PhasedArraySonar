88cd9d3d67385a9f66113f9b52cc8d1ecd84906d
Kenpil <42032640+Kenpil@users.noreply.github.com>
2019-11-09 12:22:16 +0900

    Update ADCReadMbed.cpp

--- UltrasonicSensor/ADCReadMbed.cpp
+++ UltrasonicSensor/ADCReadMbed.cpp
@@ -1,5 +1,5 @@
 #include "mbed.h"
- 
+ //ボード選択に注意！！
 SPI spi(D11, D12, D13); // mosi, miso, sclk
 DigitalOut cs(D10);//chip select?
 Serial pc(USBTX, USBRX);
@@ -8,7 +8,7 @@ Serial pc(USBTX, USBRX);
 int main()
 {
     
-    spi.format(7, 0);
+    spi.format(8, 0);
     spi.frequency(1000000);
  
     printf("Starting MCP3208\n");
@@ -17,16 +17,17 @@ int main()
     {
         cs = 0;
  
-        spi.write(0x60);
+        spi.write(0x06);
         
         uint8_t d1 = spi.write(0x00);
         uint8_t d2 = spi.write(0x00);
-        uint16_t d = (d1 << 8) | d2;
+        //uint16_t d = (d1 << 8) | d2;
+        uint16_t d = (d1&0x0f)*0x100+d2;
  
         float volt = 3.3 * (float)d / 4095;
 
         pc.printf("received\n");
-        printf("d=%d temp=%5.2f\n", d, volt * 100);
+        printf("d=%d temp=%5.2f\n", d, volt);
 
         cs = 1;
         wait(1);

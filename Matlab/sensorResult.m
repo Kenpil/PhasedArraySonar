bd2656e5d9a1551ed0402f61b491b6b641adaf6b
Kenpil <42032640+Kenpil@users.noreply.github.com>
2020-03-17 17:43:28 +0900

    Add files via upload

--- Matlab/sensorResult.m
+++ Matlab/sensorResult.m
@@ -7,24 +7,26 @@ im = 0i;
 f = 40000;
 fs = 90000;
 resultLength = 901;
-point = round(resultLength/2);
+point = round(resultLength/10);
 arrayNum = 8;
-arraivalAngle = -1/6*pi;
+arraivalAngle = -1/3*pi;
 v = 340;
 lambda = v/f;
-d = 2 * lambda;
+d = lambda/2;
 fileID = fopen('resultList.txt','w');
 
-result = zeros(resultLength, arrayNum);
+result = zeros(round(resultLength/4), arrayNum);
 
 wave = 0;
 wavePhase = 0;
-for j = 0:20
-    wave = sin(2*pi*f*j/fs);
+repeat = 36;
+phaseList = zeros(repeat+1, arrayNum);
+for j = 0:repeat
     for i = 1:arrayNum
         wavePhase = 2*pi*(f*j/fs + (i-1)*d*sin(arraivalAngle)/lambda);
-        if wavePhase > 0 && wavePhase < 16 * pi
+        if wavePhase > 0 && wavePhase < 16.1 * pi
             result(point+j, i) = sin(wavePhase);
+             phaseList(j+1, i) = wavePhase/(2*pi);
         end
     end
 end
